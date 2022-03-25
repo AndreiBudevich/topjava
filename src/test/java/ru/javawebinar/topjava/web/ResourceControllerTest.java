83ae8868c69e3585080b64e8311082d53aee768a
Andrei Budevich <budevichac01@gmail.com>
2022-03-25 14:42:16 +0300

    refactored ResourceControllerTest

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +12,7 @@ public class ResourceControllerTest extends AbstractControllerTest {
     @Test
     void getResourcesCss() throws Exception {
         perform(MockMvcRequestBuilders.get("/resources/css/style.css"))
+                .andDo(print())
                 .andExpectAll(status().isOk(), content().contentTypeCompatibleWith("text/css;charset=UTF-8"))
                 .andReturn();
     }
