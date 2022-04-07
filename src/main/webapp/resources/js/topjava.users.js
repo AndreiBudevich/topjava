bfaea9b91a0c863397ecd4178a57c1050d42caeb
Andrei Budevich <budevichac01@gmail.com>
2022-04-07 17:13:25 +0300

    refactored topjava.users.js

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -44,7 +44,7 @@ function enable(checkbox) {
         checkbox.closest('tr').attr("data-user-enabled", enable);
         successNoty(enable ? "Record enabled" : "Record disabled");
     }).fail(function () {
-        checkbox.prop('checked', !checkbox.is(":checked"));
+        checkbox.prop('checked', !enable);
         successNoty("didn't change");
     })
 }
