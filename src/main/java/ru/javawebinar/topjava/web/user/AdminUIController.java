be6a9d809c4ad24434af5dc4f3482c02382556eb
Andrei Budevich <budevichac01@gmail.com>
2022-04-11 16:10:45 +0300

    refactored Controller

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -11,7 +11,7 @@ import ru.javawebinar.topjava.to.UserTo;
 import javax.validation.Valid;
 import java.util.List;
 
-import static ru.javawebinar.topjava.util.exception.ResponseErrors.ResponseErrorMsg;
+import static ru.javawebinar.topjava.util.ValidationUtil.getErrorMsg;
 
 @RestController
 @RequestMapping(value = "/admin/users", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -40,7 +40,7 @@ public class AdminUIController extends AbstractUserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<String> createOrUpdate(@Valid UserTo userTo, BindingResult result) {
         if (result.hasErrors()) {
-            return ResponseErrorMsg(result);
+            return getErrorMsg(result);
         }
         if (userTo.isNew()) {
             super.create(userTo);
